fa1dd90ab9c7b6d607f7072fa40f5550edc3d381
avtandil shaburishvili <a.shaburishvili@gmail.com>
2022-02-15 12:55:48 +0400

    dinamic user info in parameters

--- Services/Utils.tsx
+++ Services/Utils.tsx
@@ -17,5 +17,22 @@ export const formatNumber = (number: any) => {
         .toFixed(2)
         .toString()
         .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-}       
+}
+
+export const formatDate = (dateString: string) => {
+    if (!dateString) return "";
+    let dateObj = new Date(dateString);
+    let month = dateObj.getUTCMonth() + 1; //months from 1-12
+    let day = dateObj.getUTCDate();
+    let year = dateObj.getUTCFullYear();
+    let minutes = dateObj.getMinutes();
+    let hour = dateObj.getHours();
+    let newdate =
+        ("0" + day).slice(-2) +
+        "." +
+        ("0" + month).slice(-2) +
+        "." +
+        year 
+    return newdate;
+};
 
