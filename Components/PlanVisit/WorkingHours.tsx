62946c5dc5f9bbc3c75de5173af77ebaac52a322
eto <eteri_ivanelashvili@iliauni.edu.ge>
2022-02-18 15:26:43 +0400

    lightmode

--- Components/PlanVisit/WorkingHours.tsx
+++ Components/PlanVisit/WorkingHours.tsx
@@ -1,45 +1,121 @@
-import React, { useContext } from 'react';
-import { ActivityIndicator, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { AppContext } from '../../AppContext/AppContext';
-import { Colors } from '../../Colors/Colors';
-import { useDimension } from '../../Hooks/UseDimension';
+import React, {useContext} from 'react';
+import {
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {AppContext} from '../../AppContext/AppContext';
+import {Colors} from '../../Colors/Colors';
+import {useDimension} from '../../Hooks/UseDimension';
 
-const WorkingHours = ({ data, routeId }: any) => {
-  const { state } = useContext(AppContext);
-  const { isDarkTheme } = state;
-  if(!data.length)
-  return    <ActivityIndicator style={{alignSelf: 'flex-start'}} color={'#ffffff'} />
+const WorkingHours = ({data, routeId}: any) => {
+  const {state} = useContext(AppContext);
+  const {isDarkTheme} = state;
+  if (!data.length)
+    return (
+      <ActivityIndicator style={{alignSelf: 'flex-start'}} color={'#ffffff'} />
+    );
   return (
-    <View style={{backgroundColor: isDarkTheme ? Colors.black : Colors.white }}>
-      {
-        routeId === 1 ?
-          <>
-            <Text style={[styles.text,{color: isDarkTheme ? Colors.white : Colors.black}]}>
-              მისამართი: <Text style={[styles.subTxt,{color: isDarkTheme ? Colors.white : Colors.black}]}>{data[1]['address']} </Text>
+    <View style={{backgroundColor: isDarkTheme ? Colors.black : Colors.white}}>
+      {routeId === 1 ? (
+        <>
+          <Text
+            style={[
+              styles.text,
+              {color: isDarkTheme ? Colors.white : Colors.black},
+            ]}>
+            მისამართი:{' '}
+            <Text
+              style={[
+                styles.subTxt,
+                {color: isDarkTheme ? Colors.white : Colors.black},
+              ]}>
+              {data[1]['address']}{' '}
             </Text>
-            <Text style={[styles.text,{color: isDarkTheme ? Colors.white : Colors.black}]}>
-              სამუშაო საათები: <Text style={[styles.subTxt,{color: isDarkTheme ? Colors.white : Colors.black}]}>{data[1]['working-hours']} </Text>
+          </Text>
+          <Text
+            style={[
+              styles.text,
+              {color: isDarkTheme ? Colors.white : Colors.black},
+            ]}>
+            სამუშაო საათები:{' '}
+            <Text
+              style={[
+                styles.subTxt,
+                {color: isDarkTheme ? Colors.white : Colors.black},
+              ]}>
+              {data[1]['working-hours']}{' '}
             </Text>
-          </>
-          :
-          <>
-            <Text style={[styles.text,{color: isDarkTheme ? Colors.white : Colors.black}]}>
-              მისამართი: <Text style={[styles.subTxt,{color: isDarkTheme ? Colors.white : Colors.black}]}>{data[0]['address']} </Text>
+          </Text>
+        </>
+      ) : (
+        <>
+          <Text
+            style={[
+              styles.text,
+              {color: isDarkTheme ? Colors.white : Colors.black},
+            ]}>
+            მისამართი:{' '}
+            <Text
+              style={[
+                styles.subTxt,
+                {color: isDarkTheme ? Colors.white : Colors.black},
+              ]}>
+              {data[0]['address']}{' '}
             </Text>
-            <Text style={[styles.text,{color: isDarkTheme ? Colors.white : Colors.black}]}>
-              სამუშაო საათები: <Text style={[styles.subTxt,{color: isDarkTheme ? Colors.white : Colors.black}]}>{data[0]['working-hours']} </Text>
+          </Text>
+          <Text
+            style={[
+              styles.text,
+              {color: isDarkTheme ? Colors.white : Colors.black},
+            ]}>
+            სამუშაო საათები:{' '}
+            <Text
+              style={[
+                styles.subTxt,
+                {color: isDarkTheme ? Colors.white : Colors.black},
+              ]}>
+              {data[0]['working-hours']}{' '}
             </Text>
-          </>
-      }
-{/* 
+          </Text>
+        </>
+      )}
+      {/* 
       <Text style={styles.text}>
         ტელეფონი: <Text style={styles.subTxt}>+995 595 355 033</Text>
       </Text> */}
       <View style={styles.iconView}>
-        <Text style={[styles.text,{color: isDarkTheme ? Colors.white : Colors.black}]}>სოც. სქელი:</Text>
-        <Image source={require('../../assets/images/facebook.png')} />
-        <Image source={require('../../assets/images/insta.png')} />
-        <Image source={require('../../assets/images/twiteer.png')} />
+        <Text
+          style={[
+            styles.text,
+            {color: isDarkTheme ? Colors.white : Colors.black},
+          ]}>
+          სოც. სქელი:
+        </Text>
+        <Image
+          source={
+            isDarkTheme
+              ? require('../../assets/images/facebook.png')
+              : require('../../assets/images/dark-fb-icon.png')
+          }
+        />
+        <Image
+          source={
+            isDarkTheme
+              ? require('../../assets/images/insta.png')
+              : require('../../assets/images/dark-insta-icon.png')
+          }
+        />
+        <Image
+          source={
+            isDarkTheme
+              ? require('../../assets/images/twiteer.png')
+              : require('../../assets/images/dark-twit-icon.png')
+          }
+        />
       </View>
     </View>
   );
