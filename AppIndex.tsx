a15f46c30c9d53682af79762f4faab4c2fad5ad5
shavleg kakulia <kakuliash@gmail.com>
2022-03-25 11:22:48 +0400

    theme to backend

--- AppIndex.tsx
+++ AppIndex.tsx
@@ -29,6 +29,7 @@ const AppIndex = () => {
   const RegisterCommonInterceptor = () => {
     let requestInterceptor = axios.interceptors.request.use((config: any) => {
       config.headers['langcode'] = translateService.lang === default_lang_key ? 'ka-GE' : 'en-US'; // || default_lang_key;
+      config.headers['theme'] = isDarkTheme ? 'dark' : 'light';
       return config;
     });
     let responseInterceptor = axios.interceptors.response.use(
